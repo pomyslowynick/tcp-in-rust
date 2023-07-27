678d538ebc296cfbfd9e6f6ac2cf93b91322c40c
pomyslowynick <dark0mlet@gmail.com>
2023-07-27 08:26:35 +0100

    Fix the three way handshake
    * Add checksum to the packet
    * Format the code
    * Verify the ACK on ESTAB and FINWAIT1/2
    
    Signed-off-by: pomyslowynick <dark0mlet@gmail.com>

--- trust/src/tcp.rs
+++ trust/src/tcp.rs
@@ -13,7 +13,11 @@ impl State {
     fn is_synchronized(&self) -> bool {
         match *self {
             State::SynRcvd => false,
-            State::Established | State::FinWait1 | State::FinWait2 | State::Closing | State::TimeWait => true,
+            State::Established
+            | State::FinWait1
+            | State::FinWait2
+            | State::Closing
+            | State::TimeWait => true,
         }
     }
 }
@@ -114,12 +118,7 @@ impl Connection {
                 wl1: 0,
                 wl2: 0,
             },
-            tcp: etherparse::TcpHeader::new(
-                tcph.destination_port(),
-                tcph.source_port(),
-                iss,
-                wnd
-            ),
+            tcp: etherparse::TcpHeader::new(tcph.destination_port(), tcph.source_port(), iss, wnd),
             iphr: etherparse::Ipv4Header::new(0, 64, 6, iph.destination(), iph.source()),
         };
 
@@ -140,16 +139,21 @@ impl Connection {
         let mut buf = [0u8; 1500];
         self.tcp.sequence_number = self.send.nxt;
         self.tcp.acknowledgment_number = self.recv.nxt;
-        // This is done by the kernel, we don't need to calculate the checksum
-        // let payload = [0u8; 0];
-        // syn_ack.checksum = syn_ack.calc_checksum_ipv4(&ip, &payload).expect("failed to compute checksum");
+        self.tcp.checksum = self
+            .tcp
+            .calc_checksum_ipv4(&self.iphr, &[])
+            .expect("failed to compute checksum");
 
         // not sure about this min use here, shouldn't those headers be written to it? Maybe it
         // only happens in on_packet
-        let size = std::cmp::min(buf.len(), self.tcp.header_len() as usize + self.iphr.header_len() as usize + payload.len());
+        let size = std::cmp::min(
+            buf.len(),
+            self.tcp.header_len() as usize + self.iphr.header_len() as usize + payload.len(),
+        );
 
         // historically TCP datagram doesn't have information on the length, IP packet does
-        self.iphr.set_payload_len(size);
+        self.iphr
+            .set_payload_len(size - self.iphr.header_len() as usize);
         // This is some Rust magic from Jon, where the slice pointer for buf, assigned to
         // unwritten moves furter as we write to buf. So the final returned length is
         // however much space is left in the buf, after we wrote to it.
@@ -176,7 +180,7 @@ impl Connection {
         nic.send(&buf[..buf.len() - unwritten])?;
         Ok(payload_bytes)
     }
-    
+
     pub fn send_rst(&mut self, nic: &mut tun_tap::Iface) -> io::Result<()> {
         self.tcp.rst = true;
         self.tcp.sequence_number = 0;
@@ -185,7 +189,7 @@ impl Connection {
         nic.send(&[])?;
         return Ok(());
     }
-    
+
     pub fn on_packet<'a>(
         &mut self,
         nic: &mut tun_tap::Iface,
@@ -212,7 +216,7 @@ impl Connection {
         };
         // the window end, nxt + wnd
         let wend = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
-        
+
         // I think that we don't have to worry about the scenarios below
         // yet, it's mostly for future scenarios with win == 0
         // or when there's no SYN or FIN set
@@ -221,25 +225,32 @@ impl Connection {
         // Jon says that getting through this check means we have acked
         // at least one byte. This must be some property of comparing with
         // nxt, but I am still not convinced.
-        if slen == 0 {
+        let okay = if slen == 0 {
             // zero-length segment has separate rules for acceptance
             if self.recv.wnd == 0 {
                 if seqn != self.recv.nxt {
-                    return Ok(());
+                    false
+                } else {
+                    true
                 }
             } else if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend) {
-                return Ok(());
+                false
+            } else {
+                true
             }
         } else {
             if self.recv.wnd == 0 {
-                return Ok(());
-            } else if !is_between_wrapped(
-                self.recv.nxt.wrapping_sub(1),
-                seqn,
-                wend,
-            ) {
-                return Ok(());
+                false
+            } else if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend) {
+                false
+            } else {
+                true
             }
+        };
+
+        if !okay {
+            self.write(nic, &[])?;
+            return Ok(());
         }
         // We add the length of the payload to the nxt delimiter
         self.recv.nxt = seqn.wrapping_add(slen);
@@ -251,38 +262,44 @@ impl Connection {
         let ackn = tcph.acknowledgment_number();
 
         if let State::SynRcvd = self.state {
-            if !is_between_wrapped(self.send.una.wrapping_sub(1), ackn, self.send.nxt.wrapping_add(1)) {
+            if is_between_wrapped(
+                self.send.una.wrapping_sub(1),
+                ackn,
+                self.send.nxt.wrapping_add(1),
+            ) {
                 self.state = State::Established;
             } else {
                 // TODO: RST
             }
         }
 
-        if let State::Established = self.state {
+        if let State::Established | State::FinWait1 | State::FinWait2 = self.state {
             if !is_between_wrapped(self.send.una, ackn, self.send.nxt.wrapping_add(1)) {
                 return Ok(());
             }
             self.send.una = ackn;
             assert!(data.is_empty());
 
-            self.tcp.fin = true;
-            self.write(nic, &[])?;
-            self.state = State::FinWait1;
+            if let State::Established = self.state {
+                self.tcp.fin = true;
+                self.write(nic, &[])?;
+                self.state = State::FinWait1;
+            }
         }
 
         if let State::FinWait1 = self.state {
             if self.send.una == self.send.iss + 2 {
-            // our Fin has beeen acked
-            self.state = State::FinWait2;
-        }
-                // I guess this is true because data is empty (we didn't receive
-                // anything) and it's a FIN packet
-                // Also, Jon keeps saying that we acked something, so I guess the code
-                // above is making sure that we did receive an ACK?
-                //      must have ACKed our FIN, since we detected at least on acked byte 
-                //      and we have only sent on byte (the FIN)
+                // our Fin has beeen acked
                 self.state = State::FinWait2;
             }
+            // I guess this is true because data is empty (we didn't receive
+            // anything) and it's a FIN packet
+            // Also, Jon keeps saying that we acked something, so I guess the code
+            // above is making sure that we did receive an ACK?
+            //      must have ACKed our FIN, since we detected at least on acked byte
+            //      and we have only sent on byte (the FIN)
+            self.state = State::FinWait2;
+        }
 
         if tcph.fin() {
             match self.state {
@@ -290,23 +307,11 @@ impl Connection {
                     // we're done with the connection
                     self.write(nic, &[])?;
                     self.state = State::TimeWait;
-                },
+                }
                 _ => unimplemented!(),
             }
         }
 
-        if let State::FinWait2 = self.state {
-            if !tcph.fin() || !data.is_empty() {
-                unimplemented!();
-            }
-
-            self.state = State::FinWait2;
-            // must have ACKed our FIN, since we detected at least one acked byte 
-            // and we have only sent one byte (the FIN)
-            self.tcp.fin = false;
-            self.write(nic, &[])?;
-            self.state = State::TimeWait;
-        }
         return Ok(());
     }
 }
